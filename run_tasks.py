dbbc5ba4c44dfaeac60e4c0ba5abc7dfb8f4e60f
Tuan Ngo <tuannm79@gmail.com>
2020-01-20 16:57:30 +0700

    with backend

--- run_tasks.py
+++ run_tasks.py
@@ -1,3 +1,18 @@
 from tasks import add
 
-add.delay(2, 2)
+# add.delay(2, 2)
+# -> celery.result.AsyncResult
+
+# add.apply_async((3, 3), countdown=10)
+
+# add.signature((2, 2), countdown=10)
+# add.s(2, 2)
+# -> celery.canvas.Signature
+
+# s1 = add.s(2, 3)
+# s1.delay()
+
+
+s2 = add.s(2)
+res = s2.delay(8)
+res.get()
--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
-app = Celery('tasks', broker='redis://redis:6379/0')
+# app = Celery('tasks', broker='redis://redis:6379/0')
+app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
 
 @app.task()
